aed9707d7b4c1c8abfc16f56e21c771694ba92c1
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-09-05 23:47:38 +0300

    Do not show connect button for wallet

--- src/lib/components/Section.tsx
+++ src/lib/components/Section.tsx
@@ -2,6 +2,7 @@ import classes from "./../style.module.css";
 
 interface SectionProps {
   title: string;
+  showButton?: boolean;
   buttonLabel?: string;
   onClick: () => void;
   info: (JSX.Element | string)[];
@@ -9,6 +10,7 @@ interface SectionProps {
 
 export default function Section({
   title,
+  showButton = true,
   buttonLabel,
   onClick,
   info,
@@ -17,13 +19,15 @@ export default function Section({
     <section className={classes.reqSection}>
       <header>
         <h3>{title}</h3>
-        <button
-          className={classes.hypeButton}
-          onClick={onClick}
-          disabled={!!buttonLabel}
-        >
-          {buttonLabel || "Connect"}
-        </button>
+        {showButton && (
+          <button
+            className={classes.hypeButton}
+            onClick={onClick}
+            disabled={!!buttonLabel}
+          >
+            {buttonLabel || "Connect"}
+          </button>
+        )}
       </header>
       {info.map((el, i) => (
         <span key={i} className={classes.sectionInfo}>
