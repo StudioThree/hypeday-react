85a444bc76104759bafdea0d0f6861e97384a9db
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-09-06 00:01:26 +0300

    Explicitly set input font color

--- dist/hypeday-react.js
+++ dist/hypeday-react.js
@@ -1,41 +1,41 @@
-import e, { useState as g, useEffect as d } from "react";
-const b = "_wrapper_1fxo5_1", R = "_reqSection_1fxo5_37", q = "_sectionInfo_1fxo5_49", p = "_responseInput_1fxo5_60", _ = "_hypeButton_1fxo5_72", y = "_registerButton_1fxo5_99", i = {
+import e, { useState as f, useEffect as d } from "react";
+const b = "_wrapper_l5wcw_1", R = "_reqSection_l5wcw_37", w = "_sectionInfo_l5wcw_49", q = "_responseInput_l5wcw_60", p = "_hypeButton_l5wcw_73", _ = "_registerButton_l5wcw_100", i = {
   wrapper: b,
   reqSection: R,
-  sectionInfo: q,
-  responseInput: p,
-  hypeButton: _,
-  registerButton: y
+  sectionInfo: w,
+  responseInput: q,
+  hypeButton: p,
+  registerButton: _
 };
 function E({
   title: t,
   showButton: n = !0,
-  buttonLabel: l,
-  onClick: r,
+  buttonLabel: r,
+  onClick: l,
   info: s
 }) {
   return /* @__PURE__ */ e.createElement("section", {
     className: i.reqSection
   }, /* @__PURE__ */ e.createElement("header", null, /* @__PURE__ */ e.createElement("h3", null, t), n && /* @__PURE__ */ e.createElement("button", {
     className: i.hypeButton,
-    onClick: r,
-    disabled: !!l
-  }, l || "Connect")), s.map((u, a) => /* @__PURE__ */ e.createElement("span", {
+    onClick: l,
+    disabled: !!r
+  }, r || "Connect")), s.map((u, a) => /* @__PURE__ */ e.createElement("span", {
     key: a,
     className: i.sectionInfo
   }, u)));
 }
-function k({
+function y({
   projectData: t
 }) {
   var a, o, m;
   if (!((a = t == null ? void 0 : t.discord) != null && a.enabled) && !((o = t == null ? void 0 : t.discord2) != null && o.enabled))
     return null;
-  const { discord: n, discord2: l, userInfo: r } = t, s = [];
-  n != null && n.enabled && s.push(n), l != null && l.enabled && s.push(l);
+  const { discord: n, discord2: r, userInfo: l } = t, s = [];
+  n != null && n.enabled && s.push(n), r != null && r.enabled && s.push(r);
   const u = [];
   return s.forEach((c) => {
-    var f;
+    var g;
     if (c.isServerRequired) {
       const h = /* @__PURE__ */ e.createElement(e.Fragment, null, "Join the", " ", /* @__PURE__ */ e.createElement("a", {
         href: c == null ? void 0 : c.serverLink,
@@ -45,21 +45,21 @@ function k({
       u.push(h);
     }
     if (c.isRoleRequired) {
-      const h = /* @__PURE__ */ e.createElement(e.Fragment, null, 'and have the "', (f = c == null ? void 0 : c.roleDisplayNames) == null ? void 0 : f.join(", "), '" role(s)');
+      const h = /* @__PURE__ */ e.createElement(e.Fragment, null, 'and have the "', (g = c == null ? void 0 : c.roleDisplayNames) == null ? void 0 : g.join(", "), '" role(s)');
       u.push(h);
     }
   }), /* @__PURE__ */ e.createElement(E, {
     title: "Discord",
     onClick: () => console.log("discord button clicked"),
     info: u,
-    buttonLabel: (m = r == null ? void 0 : r.discord) == null ? void 0 : m.username
+    buttonLabel: (m = l == null ? void 0 : l.discord) == null ? void 0 : m.username
   });
 }
-function F({
+function k({
   projectData: t
 }) {
-  var l;
-  if (!((l = t == null ? void 0 : t.email) != null && l.isEmailRequired))
+  var r;
+  if (!((r = t == null ? void 0 : t.email) != null && r.isEmailRequired))
     return null;
   const { userInfo: n } = t;
   return /* @__PURE__ */ e.createElement(E, {
@@ -69,11 +69,11 @@ function F({
     buttonLabel: n == null ? void 0 : n.email
   });
 }
-function S({
+function F({
   projectData: t
 }) {
-  var l;
-  if (!((l = t == null ? void 0 : t.customfield) != null && l.enabled))
+  var r;
+  if (!((r = t == null ? void 0 : t.customfield) != null && r.enabled))
     return null;
   const { customfield: n } = t;
   return /* @__PURE__ */ e.createElement("section", {
@@ -86,16 +86,16 @@ function S({
     placeholder: n.labels[0]
   }));
 }
-function w({
+function S({
   projectData: t
 }) {
   var s, u, a;
   if (!((s = t == null ? void 0 : t.twitter) != null && s.enabled))
     return null;
-  const { twitter: n, userInfo: l } = t, r = [];
+  const { twitter: n, userInfo: r } = t, l = [];
   if (n != null && n.isAccountCreatedRequired) {
     const o = /* @__PURE__ */ e.createElement(e.Fragment, null, "Have an account created before", " ", /* @__PURE__ */ e.createElement("strong", null, new Date((n == null ? void 0 : n.accountCreated) || "").toLocaleDateString()));
-    r.push(o);
+    l.push(o);
   }
   if (n != null && n.isFollowRequired) {
     const o = (u = n == null ? void 0 : n.follow) == null ? void 0 : u.split(","), m = /* @__PURE__ */ e.createElement(e.Fragment, null, "Follow these account(s):", /* @__PURE__ */ e.createElement("ul", null, o == null ? void 0 : o.map((c) => /* @__PURE__ */ e.createElement("li", {
@@ -105,11 +105,11 @@ function w({
       target: "_blank",
       rel: "noreferrer"
     }, /* @__PURE__ */ e.createElement("strong", null, "@", c))))));
-    r.push(m);
+    l.push(m);
   }
   if (n != null && n.isMinFollowersRequired) {
     const o = /* @__PURE__ */ e.createElement(e.Fragment, null, "Have at least ", /* @__PURE__ */ e.createElement("b", null, n.minFollowers, " follower(s)"));
-    r.push(o);
+    l.push(o);
   }
   if (n != null && n.isRetweetRequired) {
     const o = /* @__PURE__ */ e.createElement(e.Fragment, null, "Retweet", " ", /* @__PURE__ */ e.createElement("a", {
@@ -117,13 +117,13 @@ function w({
       target: "_blank",
       rel: "noreferrer"
     }, /* @__PURE__ */ e.createElement("strong", null, "this tweet")));
-    r.push(o);
+    l.push(o);
   }
   return /* @__PURE__ */ e.createElement(E, {
     title: "Twitter",
     onClick: () => console.log("twitter button clicked"),
-    info: r,
-    buttonLabel: (a = l == null ? void 0 : l.twitter) == null ? void 0 : a.username
+    info: l,
+    buttonLabel: (a = r == null ? void 0 : r.twitter) == null ? void 0 : a.username
   });
 }
 const v = {
@@ -136,61 +136,61 @@ const v = {
 function B({
   projectData: t
 }) {
-  var r;
+  var l;
   if (!(t != null && t.wallet))
     return null;
-  const { wallet: n } = t, l = [];
+  const { wallet: n } = t, r = [];
   if (n != null && n.isBalanceRequired) {
     const s = /* @__PURE__ */ e.createElement(e.Fragment, null, "Have at least", /* @__PURE__ */ e.createElement("strong", null, " " + n.balance + " " + v[t.chain] + " "), "in your wallet");
-    l.push(s);
+    r.push(s);
   }
   if (n != null && n.isNftRequired) {
-    const { nftCollection: s } = n, u = /* @__PURE__ */ e.createElement(e.Fragment, null, "Hold a ", /* @__PURE__ */ e.createElement("strong", null, (r = s == null ? void 0 : s.chain) == null ? void 0 : r.toUpperCase()), " NFT from", " ", /* @__PURE__ */ e.createElement("strong", null, /* @__PURE__ */ e.createElement("a", {
+    const { nftCollection: s } = n, u = /* @__PURE__ */ e.createElement(e.Fragment, null, "Hold a ", /* @__PURE__ */ e.createElement("strong", null, (l = s == null ? void 0 : s.chain) == null ? void 0 : l.toUpperCase()), " NFT from", " ", /* @__PURE__ */ e.createElement("strong", null, /* @__PURE__ */ e.createElement("a", {
       href: s == null ? void 0 : s.url,
       target: "_blank",
       rel: "noreferrer"
     }, s == null ? void 0 : s.name)));
-    l.push(u);
+    r.push(u);
   }
   return /* @__PURE__ */ e.createElement(E, {
     title: "Wallet",
     onClick: () => console.log("wallet button clicked"),
-    info: l,
+    info: r,
     showButton: !1
   });
 }
 function I({
   appId: t,
   projectId: n,
-  userId: l
+  userId: r
 }) {
   var a;
-  const [r, s] = g();
+  const [l, s] = f();
   d(() => {
     !n || fetch(
-      `https://apitest.hype.day/getProject?appId=${t}&projectid=${n}&userid=${l}`
+      `https://apitest.hype.day/getProject?appId=${t}&projectid=${n}&userid=${r}`
     ).then((o) => o.json()).then((o) => s(o));
   }, []);
   const u = () => {
     var o;
-    (o = r == null ? void 0 : r.userInfo) != null && o.registered || console.log("register button clicked");
+    (o = l == null ? void 0 : l.userInfo) != null && o.registered || console.log("register button clicked");
   };
   return /* @__PURE__ */ e.createElement("div", {
     className: i.wrapper
   }, /* @__PURE__ */ e.createElement(B, {
-    projectData: r
-  }), /* @__PURE__ */ e.createElement(w, {
-    projectData: r
-  }), /* @__PURE__ */ e.createElement(k, {
-    projectData: r
-  }), /* @__PURE__ */ e.createElement(F, {
-    projectData: r
+    projectData: l
   }), /* @__PURE__ */ e.createElement(S, {
-    projectData: r
+    projectData: l
+  }), /* @__PURE__ */ e.createElement(y, {
+    projectData: l
+  }), /* @__PURE__ */ e.createElement(k, {
+    projectData: l
+  }), /* @__PURE__ */ e.createElement(F, {
+    projectData: l
   }), /* @__PURE__ */ e.createElement("button", {
     className: i.registerButton,
     onClick: u
-  }, (a = r == null ? void 0 : r.userInfo) != null && a.registered ? "Registered! \u{1F389}" : "Click to Register"));
+  }, (a = l == null ? void 0 : l.userInfo) != null && a.registered ? "Registered! \u{1F389}" : "Click to Register"));
 }
 export {
   I as default
