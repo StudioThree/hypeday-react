67a081110a3bce96f5a6eb8de18d11ede8c8eaa5
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-09-12 22:13:19 +0300

    Get walletAddress and projectId from URL search params

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import HypeDayReact from "hypeday-react";
 import "hypeday-react/dist/style.css";
 import "./App.css";
 
 function App() {
+  const [projectId, setProjectId] = useState("OqdTSUuzrCIbapvSQWbL");
+  const [walletAddress, setWalletAddress] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setProjectId(params.get("projectId") || "OqdTSUuzrCIbapvSQWbL");
+    setWalletAddress(params.get("walletAddress") || "");
+  }, []);
+
   return (
     <div className="App">
-      <HypeDayReact appId="testId" projectId="OqdTSUuzrCIbapvSQWbL" />
+      <HypeDayReact
+        appId="9zUeQuZ2IYUVR6DOousn"
+        projectId={projectId}
+        walletAddress={walletAddress}
+      />
     </div>
   );
 }
